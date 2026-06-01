a9a6cc66308840c8a766922ba69a37bafcd16a02
Denzel Owusu-Sekyere <dudedenzy@gmail.com>
2026-06-01 12:49:55 +1000

    double click to add note

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 "use strict";
 
-let notes = document.getElementsByClassName("note");
-Array.from(notes).forEach(regsisterDrag);
+class Sticky {
+    constructor(doc) {
+        this.textArea = doc.createElement("textarea");
+        this.textArea.classList.add("note");
+        this.textArea.placeholder = "Type your notes here!"
+        this.textArea.style.left = `${Math.random() * window.innerWidth}px`
+        this.textArea.style.top = `${Math.random() * window.innerHeight}px`
+        regsisterDrag(this.textArea);
+        doc.body.appendChild(this.textArea);
+    }
+}
+
+//let notes = document.getElementsByClassName("note");
+//for (let n of notes) regsisterDrag(n);
+
+let createdNotes = new Array();
+createdNotes.push(new Sticky(document));
+
+document.addEventListener("dblclick", ev => {
+    createdNotes.push(new Sticky(document));
+});
 
 //https://stackoverflow.com/questions/24050738/javascript-how-to-dynamically-move-div-by-clicking-and-dragging
 
